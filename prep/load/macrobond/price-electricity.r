bec5e7d6329a067f599243035f58e4c423664141
peter <peter.reschenhofer@gmail.com>
2022-09-30 10:58:25 +0200

    adding volume, restructering data dir

---
+++ prep/load/macrobond/price-electricity.r
@@ -0,0 +1,24 @@
+# - INIT -----------------------------------------------------------------------
+rm(list = ls())
+source('load/macrobond/_shared.r')
+# loadPackages()
+
+
+# - DOIT -----------------------------------------------------------------------
+c.series = c(
+    atelspotbase = "base",
+    atelspotpeak = "peak"
+)
+
+d.raw = getMacrobondData(names(c.series))
+setnames(d.raw, names(c.series), c.series)
+
+d.plot = melt(d.raw, id.vars = "date")
+d.plot[, (paste0('rm', 7)) := rollmean(value, 7, fill = NA, align = "right"), by=variable]
+d.plot = d.plot[date >= "2019-01-01" & !is.na(rm7)]
+d.plot[, value := NULL]
+dates2PlotDates(d.plot)
+
+# Save
+fwrite(d.plot, file.path(g$d$wd, 'electricity', 'price-daily.csv'))
+
--- prep/load/macrobond/volume-electricity.r
+++ prep/load/macrobond/volume-electricity.r
@@ -5,25 +5,20 @@ source('load/macrobond/_shared.r')
 
 
 # - DOIT -----------------------------------------------------------------------
-# atelspotbase	    Daily	Austria, Electricity, AT, Day Base Price, EUR
-# atelspotpeak	    Daily	Austria, Electricity, AT, Day Peak Price, EUR
-# atelspotbasevol	Daily	Austria, Electricity, AT, Day Base Volume
-# atelspotpeakvol	Daily	Austria, Electricity, AT, Day Peak Volume
-
 c.series = c(
-    atelspotbase = "base",
-    atelspotpeak = "peak"
+    atelspotbasevol = "base",
+    atelspotpeakvol = "peak"
 )
 
 d.raw = getMacrobondData(names(c.series))
 setnames(d.raw, names(c.series), c.series)
 
 d.plot = melt(d.raw, id.vars = "date")
-d.plot[, (paste0('rm', 7)) := rollmean(value, 7, fill = NA), by=variable]
+d.plot[, (paste0('rm', 7)) := rollmean(value, 7, fill = NA, align = "right"), by=variable]
 d.plot = d.plot[date >= "2019-01-01" & !is.na(rm7)]
 d.plot[, value := NULL]
 dates2PlotDates(d.plot)
 
 # Save
-fwrite(d.plot, file.path(g$d$wd, 'price-electricity', 'data.csv'))
+fwrite(d.plot, file.path(g$d$wd, 'electricity', 'volume-daily.csv'))
 
