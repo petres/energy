ac3b7021cce36475bd6be652c4a83325ee400de2
petres <peter.reschenhofer@gmail.com>
2022-10-09 13:15:42 +0200

    Adding gas price raw data

--- prep/load/gas-price.r
+++ prep/load/gas-price.r
@@ -14,6 +14,7 @@ price = sapply(l.base[[4]]$data, `[[`, 2)
 price[sapply(price, is.null)] = list(0)
 
 d.raw = data.table(date = as.Date(as_datetime(date/1000)), price = unlist(price))
+# fwrite(d.raw[date >= "2019-01-01"], file.path('data', 'gas-price.csv'))
 
 d.plot = prepData(d.raw)
 
