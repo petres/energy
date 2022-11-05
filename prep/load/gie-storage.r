60bcc5bdb4184158d7e01058a2f015e629ea748b
Johannes Schmidt <johannes.schmidt@boku.ac.at>
2022-11-05 16:07:36 +0100

    Added download EU storage (#14)
    * Added download EU
    
    * Removed figure

--- prep/load/gie-storage.r
+++ prep/load/gie-storage.r
@@ -8,34 +8,48 @@ loadPackages(
 
 # - DOIT -----------------------------------------------------------------------
 # Load
-loadGieData = function(date = Sys.Date(), page = 1) {
+loadGieData = function(date = Sys.Date(), page = 1, country = "AT") {
     l(glue("page: {page}"))
-    url = glue("https://agsi.gie.eu/api?country=AT&from=2018-12-01&to={date}&size=300&page={page}")
+    url = glue("https://agsi.gie.eu/api?country={country}&from=2018-12-01&to={date}&size=300&page={page}")
+    if(country == "EU"){
+        url = glue("https://agsi.gie.eu/api?type={country}&from=2018-12-01&to={date}&size=300&page={page}")
+    }
     t = getURL(url, httpheader = c("x-key" = g$gie$params$key))
     # NO VALID JSON, HOW THEY ARE ABLE TO PRODUCE SOMETHING LIKE THAT
     t = sub("<a href=\"/data-overview/eu\">EU</a> > AT", "", t, fixed = TRUE)
     fromJSON(t)
 }
 
-page = 1
-d.base = data.table()
-while(TRUE) {
-    l.d = loadGieData(page = page)
-    d.base = rbind(d.base, l.d$data)
-    if (l.d$last_page == page)
-        break;
-    page = page + 1
+loadGieDataAllPages = function(date = Sys.Date(), country = "AT"){
+
+    page = 1
+    d.base = data.table()
+    while(TRUE) {
+        l.d = loadGieData(date, page = page, country)
+        d.base = rbind(d.base, l.d$data)
+        if (l.d$last_page == page)
+            break;
+        page = page + 1
+    }
+
+    d.base = as.data.table(d.base)
+
+    return(d.base)
+
 }
 
-d.base = as.data.table(d.base)
+d.base = loadGieDataAllPages()
+d.base = rbind(d.base, loadGieDataAllPages(country = "EU"))
+
 
 # Save
-saveRDS(d.base, file.path(g$d$o, "storage.rData"))
+saveRDS(d.base.at, file.path(g$d$o, "storage.at.rData"))
 
 # Plot, Preparation
 d.plot = d.base[, .(
     date = as.Date(gasDayStart),
-    value = as.numeric(gasInStorage)
+    value = as.numeric(gasInStorage),
+    country = name
 )][order(date)]
 dates2PlotDates(d.plot)
 
